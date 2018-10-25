f19b897ac65ad0a2ef977b11eb218b6e2a3f1f63
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-10-25 21:17:54 +0100

    Update sim/mt19937_32/mt19937_32.go

--- sim/mt19937_32/mt19937_32.go
+++ sim/mt19937_32/mt19937_32.go
@@ -1,9 +1,8 @@
 package mt19937_32
 
-/*
-	The following license applies to this file:
+/*	The following license applies to this file:
 
-	Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+	Copyright (c) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura.
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
@@ -11,15 +10,15 @@ package mt19937_32
 	are met:
 
 	1. Redistributions of source code must retain the above copyright
-	   notice, this list of conditions and the following disclaimer.
+	notice, this list of conditions and the following disclaimer.
 
 	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
 
 	3. The names of its contributors may not be used to endorse or promote
-	   products derived from this software without specific prior written
-	   permission.
+	products derived from this software without specific prior written
+	permission.
 
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
